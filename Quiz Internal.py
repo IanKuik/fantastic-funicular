92d7fb36fc9457b0de694d34854b9290499ad72b
Ian Kuik <ian.kuik@.elim.school.nz>
2018-05-08 14:34:33 +1200

    test 2

--- Quiz Internal.py
+++ Quiz Internal.py
@@ -1,12 +1,13 @@
 def verify():
-    answer_list = ["wargames", "captain america the winter soldier", "1977", "die hard", "john mclane", "xenommorph"]
+    answer_list = ["wargames", "captain america the winter soldier", "1977", "die hard", "john mclane", "xenomorph"]
     if answer in answer_list:
         print("Correct!")
         outcome = "correct"
 
     if outcome == "correct":
         print("Next Question!")
-
+    else:
+        outcome == "incorrect"
 
 
 
@@ -17,6 +18,8 @@ if begin == "yes":
     #ask first question
     answer = input("First of all, what movie was that line from? ")
     verify()
+    if outcome != "incorrect":
+        print("Wrong. It was ".format(answer_list[0]))
 
     #Question 2
     print()
@@ -27,10 +30,13 @@ if begin == "yes":
     print()
     answer = input("What is Jake Peralta's favorite movie? ")
     verify()
+    if outcome != "incorrect":
+        print("Wrong. It was ".format(answer_list[0]))
+
 
     #question 4
     print()
-    answer = input("Follow up question: What is the main protagonist's name in the film Die Hard? ")
+    answer = input("Follow up question: What is the main protagonist's name in the film; 'Die Hard'? ")
     verify()
 
     #question 5
